d9d303f580b067c9b8b5f2fa59ad4f604536622e
Naveen Bhusare <79650422+Naveen-7000@users.noreply.github.com>
2023-06-27 17:48:58 +0530

    fixed routing issue

--- pages/index.js
+++ pages/index.js
@@ -3,57 +3,55 @@ import Feature from "@/components/Feature";
 import NewProducts from "@/components/NewProducts";
 import { useContext } from "react";
 import UserContext from "@/components/UserContext";
-import { useRouter } from "next/router";
-import { useEffect,useState } from "react";
+import { Product } from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
+import LoginPage from "./login";
 
-export default function HomePage() {
-  const [featuredProduct, setFeaturedProduct] = useState(null);
-  const [newProducts, setNewProducts] = useState([]);
+export default function HomePage({featuredProduct, newProducts}) {
+  // const [featuredProduct, setFeaturedProduct] = useState(null);
+  // const [newProducts, setNewProducts] = useState([]);
+  // const router = useRouter();
   const {user} = useContext(UserContext);
-  const router = useRouter();
 
-  useEffect(() => {
-   (async()=>{
-    const {featuredProduct, newProducts} = await fetch('/api/product').then(res=>res.json());
-    setFeaturedProduct(featuredProduct);
-    setNewProducts(newProducts);
-   })()
-  }, []);
+  // useEffect(() => {
+  //  (async()=>{
+  //   const {featuredProduct, newProducts} = await fetch('/api/product').then(res=>res.json());
+  //   setFeaturedProduct(featuredProduct);
+  //   setNewProducts(newProducts);
+  //  })()
+  // }, []);
 
-  useEffect(()=>{
-    if(user === null) {
-      router.push("/login");
-    }
-  },[])
+  // useEffect(()=>{
+  //   if(user === null) {
+  //     router.push("/login");
+  //   }
+  // },[])
   console.log(featuredProduct,"userhome");
 
   return (
     <div>
+      {
+        user === null ? <LoginPage /> : 
+        <>
       <Header />
       <Feature product={featuredProduct} />
       <NewProducts products={newProducts} />
+        </>
+      }
     </div>
   );
 }
 
-// export async function getStaticProps() {
-//   const featuredProductId =  '64917e6124a69896fe51d71f';
-//   await mongooseConnect();
-//   const featuredProduct = await Product.findById(featuredProductId);
-//   const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
+export async function getServerSideProps() {
+  const featuredProductId =  '64917e6124a69896fe51d71f';
+  await mongooseConnect();
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
 
-//   if (user === null) {
-//     return {
-//       redirect: {
-//         destination: "/login",
-//         permanent: false,
-//       },
-//     };
-//   }
-//   return {
-//     props: {
-//       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
-//       newProducts: JSON.parse(JSON.stringify(newProducts)),
-//     },
-//   };
-// }
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
+  };
+}
