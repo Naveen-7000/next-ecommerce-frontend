14682d24e92cdf9e054d49f71fa84c77f0e65deb
Naveen Bhusare <79650422+Naveen-7000@users.noreply.github.com>
2023-06-27 13:48:10 +0530

    PUBLI_URL fixed with dynamic url

--- pages/account.js
+++ pages/account.js
@@ -21,126 +21,137 @@ const CityHolder = styled.div`
 `;
 
 export default function AccountPage() {
-
-  const {user,setUser,clearUser} = useContext(UserContext);
+  const { user, setUser, clearUser } = useContext(UserContext);
 
   const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-const [city, setCity] = useState("");
-const [postalCode, setPostalCode] = useState("");
-const [country, setCountry] = useState("");
-const [fieldsRequired, setFieldsRequired] = useState(false);
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [country, setCountry] = useState("");
+  const [fieldsRequired, setFieldsRequired] = useState(false);
 
-useEffect(() => {
-  if (user) {
-    const { name: existingName, email: existingEmail, address: existingAddress } = user;
-    setName(existingName || "");
-    setEmail(existingEmail || "");
-    setCity(existingAddress?.city || "");
-    setPostalCode(existingAddress?.pincode || "");
-    setCountry(existingAddress?.country || "");
-  }
-}, [user]);
+  useEffect(() => {
+    if (user) {
+      const {
+        name: existingName,
+        email: existingEmail,
+        address: existingAddress,
+      } = user;
+      setName(existingName || "");
+      setEmail(existingEmail || "");
+      setCity(existingAddress?.city || "");
+      setPostalCode(existingAddress?.pincode || "");
+      setCountry(existingAddress?.country || "");
+    }
+  }, [user]);
 
-useEffect(() => {
-  if (fieldsRequired) {
-    const timeout = setTimeout(() => {
-      setFieldsRequired(false);
-    }, 3000); // 3 seconds
+  useEffect(() => {
+    if (fieldsRequired) {
+      const timeout = setTimeout(() => {
+        setFieldsRequired(false);
+      }, 3000); // 3 seconds
 
-    return () => clearTimeout(timeout);
-  }
-}, [fieldsRequired]);
+      return () => clearTimeout(timeout);
+    }
+  }, [fieldsRequired]);
 
   const router = useRouter();
 
-  const handleLogout = ()=>{
+  const handleLogout = () => {
     clearUser();
     router.push("/login");
-  }
+  };
 
-  const handleUpdateUser = async(e)=>{
+  const handleUpdateUser = async (e) => {
     e.preventDefault();
-    if (!name || !email || !city || !postalCode ||  !country) {
+    if (!name || !email || !city || !postalCode || !country) {
       setFieldsRequired(true);
       return;
     }
-    if(name && email && city && postalCode && country){
-      const address = {city,pincode:postalCode,country};
-      const data = {name, email, address};
+    if (name && email && city && postalCode && country) {
+      const address = { city, pincode: postalCode, country };
+      const data = { name, email, address };
       clearUser();
-      const res = await axios.put('/api/auth/update',data);
+      const res = await axios.put("/api/auth/update", data);
       setUser(res.data?.updatedUser);
     }
-  }
+  };
   return (
     <>
       <Header />
       <Center>
-      <Box>
-              <h2>Account details</h2>
-              {
-              !user && (
-                <>
-                <p>Login first to access</p>
-                <Button block black onClick={() => router.push("/login")}>Login</Button>
-                </>
-              )
-              }
-              <Input
-                type="text"
-                placeholder="Name"
-                value={name}
-                name="name"
-                onChange={(ev) => setName(ev.target.value)}
-              />
-              <Input
-                type="text"
-                placeholder="Email"
-                value={email}
-                name="email"
-                onChange={(ev) => setEmail(ev.target.value)}
-              />
-              <CityHolder>
-                <Input
-                  type="text"
-                  placeholder="City"
-                  value={city}
-                  name="city"
-                  onChange={(ev) => setCity(ev.target.value)}
-                />
-                <Input
-                  type="text"
-                  placeholder="Postal Code"
-                  value={postalCode}
-                  name="postalCode"
-                  onChange={(ev) => setPostalCode(ev.target.value)}
-                />
-              </CityHolder>
-              <Input
-                type="text"
-                placeholder="Country"
-                value={country}
-                name="country"
-                onChange={(ev) => setCountry(ev.target.value)}
-              />
-              <p
-                style={{
-                  color: "red",
-                  display: fieldsRequired ? "block" :"none",
-                  fontSize:14,
-                  fontWeight:600
-                }}
+        <Box>
+          <h2>Account details</h2>
+          {!user && (
+            <>
+              <p>Login first to access</p>
+              <Button
+                block="true"
+                black="true"
+                onClick={() => router.push("/login")}
               >
-                Fill all the fields !
-              </p>
-              <Button black block onClick={e=>handleUpdateUser(e)} >
-                Save
+                Login
               </Button>
-              <Button green onClick={handleLogout}>
-                Logout
-              </Button>
-            </Box>
+            </>
+          )}
+          <Input
+            type="text"
+            placeholder="Name"
+            value={name}
+            name="name"
+            onChange={(ev) => setName(ev.target.value)}
+          />
+          <Input
+            type="text"
+            placeholder="Email"
+            value={email}
+            name="email"
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <CityHolder>
+            <Input
+              type="text"
+              placeholder="City"
+              value={city}
+              name="city"
+              onChange={(ev) => setCity(ev.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="Postal Code"
+              value={postalCode}
+              name="postalCode"
+              onChange={(ev) => setPostalCode(ev.target.value)}
+            />
+          </CityHolder>
+          <Input
+            type="text"
+            placeholder="Country"
+            value={country}
+            name="country"
+            onChange={(ev) => setCountry(ev.target.value)}
+          />
+          <p
+            style={{
+              color: "red",
+              display: fieldsRequired ? "block" : "none",
+              fontSize: 14,
+              fontWeight: 600,
+            }}
+          >
+            Fill all the fields !
+          </p>
+          <Button
+            black="true"
+            block="true"
+            onClick={(e) => handleUpdateUser(e)}
+          >
+            Save
+          </Button>
+          <Button green="true" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Box>
       </Center>
     </>
   );
