d20be56b34c4f5d044833442f856a7ba135c6606
Naveen Bhusare <79650422+Naveen-7000@users.noreply.github.com>
2023-10-10 23:21:47 +0530

    Fixed non existing file import

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import {createGlobalStyle} from "styled-components";
 import {CartContextProvider} from "@/components/CartContext";
 import { UserProvider } from "@/components/UserContext";
-import StyledComponentsRegistry from "@/lib/registry";
 
 const GlobalStyles = createGlobalStyle`
   body{
